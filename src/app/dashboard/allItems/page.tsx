47da10206d516fdd488fd53b878b7ba6e478bb13
Almaash <alamalmaash@gmail.com>
2025-05-23 18:56:59 +0530

    added commmmmm

--- src/app/dashboard/allItems/page.tsx
+++ src/app/dashboard/allItems/page.tsx
@@ -41,6 +41,11 @@ interface ItemVariant {
 }
 
 // connnnnnnnnnn
+// connnnnnnnnnn
+// connnnnnnnnnn
+// connnnnnnnnnn
+// connnnnnnnnnn
+// connnnnnnnnnn
 
 const ItemVariantComponent = () => {
   const [variants, setVariants] = useState<ItemVariant[]>([]);
